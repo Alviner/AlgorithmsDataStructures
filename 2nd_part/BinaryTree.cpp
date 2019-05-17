4957143285d8eeaf116e40fe24a5d49ca0cce7ba
Alviner <gr.eyed.black.fox@gmail.com>
2019-05-18 01:32:38 +1000

    fix: adding binary tree

--- 2nd_part/BinaryTree.cpp
+++ 2nd_part/BinaryTree.cpp
@@ -9,11 +9,11 @@ public:
     BSTNode *RightChild; // правый потомок
 
     BSTNode(int key, int val, BSTNode *parent) {
-        NodeKey = key;
-        NodeValue = val;
-        Parent = parent;
-        LeftChild = nullptr;
-        RightChild = nullptr;
+        this->NodeKey = key;
+        this->NodeValue = val;
+        this->Parent = parent;
+        this->LeftChild = nullptr;
+        this->RightChild = nullptr;
     }
 
     int Count() {
@@ -24,7 +24,7 @@ public:
         if (this->RightChild != nullptr) {
             res += this->RightChild->Count();
         }
-        return res; // количество узлов в поддереве
+        return res;
     }
 };
 
@@ -40,7 +40,7 @@ public:
     // true, если родительскому узлу надо добавить новый левым
     bool ToLeft;
 
-    BSTFind() { Node = nullptr; }
+    BSTFind() { this->Node = nullptr; }
 };
 
 class BST {
@@ -56,21 +56,21 @@ public:
         BSTFind *find = new BSTFind();
         find->Node = this->Root;
         while (find->Node != nullptr) {
-            if (find->Node->NodeKey == key) {
-                find->NodeHasKey = true;
-                return find;
-            } else if (find->Node->NodeKey > key) {
+            if (find->Node->NodeKey > key) {
                 find->ToLeft = true;
                 if (find->Node->LeftChild == nullptr) {
                     return find;
                 }
                 find->Node = find->Node->LeftChild;
-            } else {
+            } else if (find->Node->NodeKey < key) {
                 find->ToLeft = false;
                 if (find->Node->RightChild == nullptr) {
                     return find;
                 }
                 find->Node = find->Node->RightChild;
+            } else {
+                find->NodeHasKey = true;
+                return find;
             }
         }
         return find;
@@ -78,21 +78,25 @@ public:
 
     bool AddKeyValue(int key, int val) {
         // добавляем ключ-значение в дерево
-        BSTFind *find = this->FindNodeByKey(key);
         BSTNode *node = new BSTNode(key, val, nullptr);
-        if (find->NodeHasKey) {
-            return false; // если ключ уже есть
-        }
-        if (find->Node == nullptr) {
-            this->Root = node;
-        } else {
+        BSTFind *find = FindNodeByKey(key);
+
+        if (find->Node != nullptr) {
+            if (find->NodeHasKey) {
+                return false; // если ключ уже есть
+            }
+
             if (find->ToLeft) {
                 find->Node->LeftChild = node;
-            } else if (!find->ToLeft) {
+            }
+            if (!find->ToLeft) {
                 find->Node->RightChild = node;
             }
+            node->Parent = find->Node;
+        } else {
+            this->Root = node;
         }
-        node->Parent = find->Node;
+
         return true;
     }
 
@@ -115,63 +119,61 @@ public:
 
     bool DeleteNodeByKey(int key) {
         // удаляем узел по ключу
-
         BSTFind *find = this->FindNodeByKey(key);
-        if (!find->NodeHasKey) {
-            return false; // если узел не найден
+
+        if (find->NodeHasKey) {
+            if (find->Node->RightChild != nullptr) {
+                find->Node->RightChild->Parent = find->Node->Parent;
+
+                BSTNode *min = this->FinMinMax(find->Node->RightChild, false);
+                if (min != nullptr) {
+                    if (find->Node->LeftChild != nullptr) {
+                        find->Node->LeftChild->Parent = min;
+                    }
+                    min->LeftChild = find->Node->LeftChild;
+                }
+
+                if (find->Node->Parent == nullptr) {
+                    this->Root = find->Node->RightChild;
+                } else {
+                    if (find->ToLeft) {
+                        find->Node->Parent->LeftChild = find->Node->RightChild;
+                    } else {
+                        find->Node->Parent->RightChild = find->Node->RightChild;
+                    }
+                }
+            } else if (find->Node->LeftChild != nullptr) {
+                find->Node->LeftChild->Parent = find->Node->Parent;
+                if (find->Node->Parent == nullptr) {
+                    this->Root = find->Node->LeftChild;
+                } else {
+                    if (find->ToLeft) {
+                        find->Node->Parent->LeftChild = find->Node->LeftChild;
+                    } else {
+                        find->Node->Parent->RightChild = find->Node->LeftChild;
+                    }
+                }
+            } else {
+                if (find->Node->Parent == nullptr) {
+                    this->Root = nullptr;
+                } else {
+                    if (find->ToLeft) {
+                        find->Node->Parent->LeftChild = nullptr;
+                    } else {
+                        find->Node->Parent->RightChild = nullptr;
+                    }
+                }
+            }
+            return true;
         }
-
-        if (find->Node->RightChild != nullptr) {
-            find->Node->RightChild->Parent = find->Node->Parent;
-
-            BSTNode *min = this->FinMinMax(find->Node->RightChild, false);
-            if (min != nullptr) {
-                if (find->Node->LeftChild != nullptr) {
-                    find->Node->LeftChild->Parent = min;
-                }
-                min->LeftChild = find->Node->LeftChild;
-            }
-
-            if (find->Node->Parent == nullptr) {
-                this->Root = find->Node->RightChild;
-            } else {
-                if (find->ToLeft) {
-                    find->Node->Parent->LeftChild = find->Node->RightChild;
-                } else {
-                    find->Node->Parent->RightChild = find->Node->RightChild;
-                }
-            }
-        } else if (find->Node->LeftChild != nullptr) {
-            find->Node->LeftChild->Parent = find->Node->Parent;
-            if (find->Node->Parent == nullptr) {
-                this->Root = find->Node->LeftChild;
-            } else {
-                if (find->ToLeft) {
-                    find->Node->Parent->LeftChild = find->Node->LeftChild;
-                } else {
-                    find->Node->Parent->RightChild = find->Node->LeftChild;
-                }
-            }
-        } else {
-            if (find->Node->Parent == nullptr) {
-                this->Root = nullptr;
-            } else {
-                if (find->ToLeft) {
-                    find->Node->Parent->LeftChild = nullptr;
-                } else {
-                    find->Node->Parent->RightChild = nullptr;
-                }
-            }
-        }
-
-        return true;
+        return false; // если узел не найден
     }
 
-    int Count() { // количество узлов в дереве
-        if (this->Root != nullptr) {
-            return this->Root->Count();
+    int Count() {
+        if (this->Root == nullptr) {
+            return 0;
         }
-        return 0;
+        return this->Root->Count(); // количество узлов в дереве
     }
 
 };
